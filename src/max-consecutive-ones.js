53ebe8f1d394d832f770ce3c6f567158d0603d92
Zarar Ali <zararali1210@gmail.com>
2025-10-07 23:48:21 +0530

    merged sorted array

--- src/max-consecutive-ones.js
+++ src/max-consecutive-ones.js
@@ -1,1 +1,17 @@
 console.log("Welecome Max Consecutive Ones");
+
+var findMaxConsecutiveOnes = function (nums) {
+  let currentCount = 0;
+  let maxCount = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) {
+      currentCount++;
+    } else {
+      maxCount = Math.max(currentCount, maxCount);
+      currentCount = 0;
+    }
+  }
+  return Math.max(maxCount, currentCount);
+};
+let arr = [1, 2, 1, 1, 1, 4, 1];
+console.log(findMaxConsecutiveOnes(arr));
